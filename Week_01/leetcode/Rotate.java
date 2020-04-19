3f441e36d142ac957a9618228a55f25bc2511cb3
wangshunru <53801902+wangshunru@users.noreply.github.com>
2020-04-19 20:27:41 +0800

    189   旋转数组
    189   旋转数组

---
+++ Week_01/leetcode/Rotate.java
@@ -0,0 +1,17 @@
+public class Rotate{
+	public void rotate(int[] nums,int k){
+		k %= nums.length;
+		reverse(nums,0,nums.length-1);
+		reverse(nums,0,k-1);
+		reverse(nums,k,nums.length-1);
+	}
+	public void reverse(int[] nums,int start,int end){
+		while(start < end){
+			nums[start] = nums[start] ^ nums[end];
+			nums[end] = nums[start] ^ nums[end];
+			nums[start] = nums[start] ^ nums[end];
+			start++;
+			end--;
+		}
+	}
+}
