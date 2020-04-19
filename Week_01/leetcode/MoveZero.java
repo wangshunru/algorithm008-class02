5e8fabdaa738b0d006c50b76b9e9a8cb33e14adf
wangshunru <53801902+wangshunru@users.noreply.github.com>
2020-04-19 21:17:54 +0800

    283 移动零

---
+++ Week_01/leetcode/MoveZero.java
@@ -0,0 +1,15 @@
+//283 移动零
+public class MoveZero{
+	public void moveZero(int[] nums){
+		int j = 0;
+		for(int i = 0;i < nums.length;i++){
+			if(nums[i] != 0){
+				if(i != j){
+					nums[j] = nums[i];
+					nums[i] = 0;
+				}
+			}
+			j++;
+		}
+	}
+}
