37a33b72a13e035f672fcdcc979be3b25d224332
wangshunru <53801902+wangshunru@users.noreply.github.com>
2020-05-17 21:57:21 +0800

    Create MaxProfit.java

---
+++ Week_04/MaxProfit.java
@@ -0,0 +1,12 @@
+//买卖股票的最佳时机 II
+class Solution {
+    public int maxProfit(int[] prices) {
+        int res = 0;
+        for(int i = 0;i < prices.length-1;i++){
+            if(prices[i+1] > prices[i]){
+                res += prices[i+1] - prices[i];
+            }
+        }
+        return res;
+    }
+}
