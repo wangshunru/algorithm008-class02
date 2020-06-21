631fb227169f0286bf2300ddc04bb9d16094dd2d
wangshunru <53801902+wangshunru@users.noreply.github.com>
2020-06-21 18:13:42 +0800

    Create ReverseWords.java

---
+++ Week_08/ReverseWords.java
@@ -0,0 +1,25 @@
+public String reverseWords(String s) {
+        char[] chars = s.toCharArray();
+        int n = chars.length;
+        int left = 0;
+        int right = 0;
+        char tmp;
+        for (int i = 0; i < n; i++) {
+            if (chars[i] == ' '){
+                right = i - 1;
+                while (left < right){
+                    tmp = chars[right];
+                    chars[left++] = chars[right--];
+                    chars[right] = tmp;
+                }
+                left = i + 1;
+            }
+        }
+        right = n - 1;
+        while (left < right){
+            tmp = chars[right];
+            chars[left++] = chars[right--];
+            chars[right] = tmp;
+        }
+        return new String(chars);
+    }
