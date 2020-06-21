2bb3ab06dfc0fb43c751fe0f271b86f6a2087350
wangshunru <53801902+wangshunru@users.noreply.github.com>
2020-06-21 18:03:10 +0800

    Create ReverseOnlyLetters.java

---
+++ Week_08/ReverseOnlyLetters.java
@@ -0,0 +1,21 @@
+public String reverseOnlyLetters(String S) {
+        char[] chars = S.toCharArray();
+        if (chars.length < 2) return S;
+        int left = 0;
+        int right = chars.length-1;
+        while (left < right){
+            while (left < right && !isChar(chars[left])) left++;
+            while (left < right && !isChar(chars[right])) right--;
+            if (left < right){
+                chars[left] ^= chars[right];
+                chars[right] ^= chars[left];
+                chars[left] ^= chars[right];
+                left ++;
+                right--;
+            }
+        }
+        return new String(chars);
+    }
+    public static boolean isChar(char c){
+        return  (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+    }
